3fc9e704faa21a1bd77bec9e46d863dc5b0fc048
sumaiya02032007 <sumaiya02032007@gmail.com>
2025-12-12 17:42:16 +0530

    Add files via upload

---
+++ Fulladder.v
@@ -0,0 +1,11 @@
+module Fulladder (
+    input  wire a, b, cin,   // Inputs
+    output wire sum, carry   // Outputs
+);
+
+    // Logic equations
+    assign sum   = a ^ b ^ cin;                  // XOR for sum
+    assign carry = (a & b) | (b & cin) | (a & cin); // Majority function for carry
+
+endmodule
+
